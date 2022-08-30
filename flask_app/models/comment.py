1795127cae9d33a5195d438c20ae2873173bf51f
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-08-29 21:30:35 -0700

    added comment model

---
+++ flask_app/models/comment.py
@@ -0,0 +1,13 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash, request     
+from flask_app.models import user   
+
+class Comment:
+    db_name = "travel_mem_schema_many" 
+    def __init__( self , data ):
+        self.id = data['id']
+        self.comment = data['comment']
+        self.memory_id = data['memory_id']
+        self.user_id = data['user_id']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
